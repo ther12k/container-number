7e651f1209232ef0203bee4bfa4368fcb97c4a37
Rizky Zulkarnaen <rizkyz@gmail.com>
2022-03-21 21:20:29 +0700

    update config

--- config/host.py
+++ config/host.py
@@ -1,10 +1,10 @@
-GATE = 'gate09'
+GATE = 'GATE09'
 ID_DEVICE = '1'
 
 # FTP
-IP_FTP      = '192.168.8.200'
+IP_FTP      = '192.168.8.1'
 USER_NAME   = 'halotec'
-USER_PASSWD = 'H@lotec100$$'
+USER_PASSWD = 'halotec123'
 
-IP_API      = 'http://192.168.8.200'
+IP_API      = 'http://192.168.8.1'
 END_POINT   = 'api/v1/seal'

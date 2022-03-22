0f24456f88a0c87f7acef8fa62f9cba7ddacaf7f
Rizky Zulkarnaen <rizkyz@gmail.com>
2022-03-22 12:28:29 +0700

    host prod

--- config/host.py
+++ config/host.py
@@ -2,14 +2,13 @@ GATE_ID = 'GATE09'
 DEVICE_ID = '1'
 
 # FTP
-FTP_HOST      = '25.7.222.255'
+FTP_HOST      = '192.168.8.1'
+#FTP_HOST      = '149.129.48.59'
 USER_NAME   = 'halotec'
 USER_PASSWD = 'halotec123'
 
-#IP_API      = 'http://192.168.8.1'
-#END_POINT   = 'http://192.168.8.1/api/v1/'
-
-END_POINT   = 'http://149.129.48.59:8008/api/v1/'
+END_POINT   = 'http://192.168.8.1/api/v1/'
+#END_POINT   = 'http://149.129.48.59:8008/api/v1/'
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 TIMEID_FORMAT = "%Y%m%d%H%M%S%f"
 
